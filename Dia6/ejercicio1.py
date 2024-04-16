93f9c50c65de6f0fa4cb93e23a7a9c5983356741
Karen <karenlorenacriscaceres@gmail.com>
2024-04-16 16:54:27 -0600

    karen

--- Dia6/ejercicio1.py
+++ Dia6/ejercicio1.py
@@ -1,9 +1,9 @@
-nodos=[]
-cant=int(input(""))
-assert 0 <= cant <= 300, "Ingresaste un número fuera del rango"
-
-for i in range(0,cant):
-    nodos.split(),int(input(""))
-    repetidos = list(set(nodos))
-    repetidos.sort()
-    print(repetidos)
+# Leemos una línea de entrada que contiene números separados por espacios
+# Tomamos los primeros 300 números ingresados para cumplir con el límite
+# Convertimos cada número de cadena a entero y los almacenamos en una lista
+nodos = list(map(int,input().split()[0:300]))
+# Eliminamos duplicados convirtiendo la lista en un conjunto y luego volviéndola a convertir en lista
+# Ordenamos la lista resultante
+repetidos = sorted(set(nodos))
+#Imprimimos la lista de números sin duplicados y ordenada
+print(repetidos)
