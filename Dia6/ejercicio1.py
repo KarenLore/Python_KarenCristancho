cc94a834010dbc6fd3d439c323e6b0a221b215f8
Karen <karenlorenacriscaceres@gmail.com>
2024-04-16 18:27:44 -0600

    karen

--- Dia6/ejercicio1.py
+++ Dia6/ejercicio1.py
@@ -1,9 +1,11 @@
 # Leemos una línea de entrada que contiene números separados por espacios
 # Tomamos los primeros 300 números ingresados para cumplir con el límite
 # Convertimos cada número de cadena a entero y los almacenamos en una lista
-nodos = list(map(int,input().split()[:300]))
+nodos = list(map(int,input().split()[0:300]))
 # Eliminamos duplicados convirtiendo la lista en un conjunto y luego volviéndola a convertir en lista
 # Ordenamos la lista resultante
 repetidos = sorted(set(nodos))
 #Imprimimos la lista de números sin duplicados y ordenada
 print(repetidos)
+
+#:300 seleccionara los primeros 300 números ingresados
