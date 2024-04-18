9dab757ae454a59db6f2cd1e922103c30edbcfe2
Karen <karenlorenacriscaceres@gmail.com>
2024-04-17 23:23:41 -0600

    karen

--- Dia7/trabajo_grupal.py
+++ Dia7/trabajo_grupal.py
@@ -9,7 +9,7 @@ productos = {
 # Función para mostrar el catálogo de productos
 def mostrar_catálago():
     print("Catálago de productos:")
-    for id, producto in productos.items():
+    for id, producto in productos.items(): #items() puedes recorrer un diccionario y acceder a cada par clave-valor (es decir, producto por producto en este contexto).
         print(f"ID: {id}\nNombre: {producto['Nombre']}\nPrecio:${producto['Precio']}")
         print()
 
@@ -20,7 +20,7 @@ def agregar_al_carrito(carrito):
     
     if id_producto in productos:
         if cantidad <= productos[id_producto]["Cantidad disponible"]:
-            carrito[id_producto] = carrito.get(id_producto, 0) + cantidad
+            carrito[id_producto] = carrito.get(id_producto) + cantidad #get(), puedes acceder a los valores asociados a las claves que ya están definidas en el diccionario. Si la clave está presente en el diccionario
             print("Producto agregado al carrito.")
         else:
             print("La cantidad ingresada excede la cantidad disponible.")
@@ -82,7 +82,16 @@ def menu():
         else:
             print("Opción no válida. Intentelo de nuevo.")
 
+#Llamar la función menú
 menu()
+
+#Diccionario de Productos: Define un diccionario que almacena información sobre los productos disponibles, incluyendo el nombre, precio y cantidad disponible.
+#Función para Mostrar el Catálogo de Productos: Esta función imprime el catálogo de productos con sus respectivos ID, nombres y precios.
+#Función para Agregar Productos al Carrito: Esta función permite al usuario agregar productos al carrito de compras, solicitando el ID del producto y la cantidad deseada.
+#Función para Mostrar el Contenido del Carrito de Compras: Esta función muestra el contenido del carrito de compras, incluyendo los productos agregados y sus cantidades correspondientes.
+#Función para Calcular el Total de la Compra:  Esta función calcula el total de la compra sumando el precio de cada producto multiplicado por su cantidad en el carrito.
+#Función para el Menú Principal: Esta función representa el menú principal del programa, donde el usuario puede realizar varias acciones, como agregar productos al carrito, ver el contenido del carrito, calcular el total de la compra, finalizar la compra o salir del programa.
+
             
 
 
