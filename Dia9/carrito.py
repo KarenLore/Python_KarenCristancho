a4001951c2e02b3290a147cab41da32533da0074
karen <karnelorenacriscaceres@gmail.com>
2024-04-19 12:53:13 -0500

    karen

---
+++ Dia9/carrito.py
@@ -0,0 +1,21 @@
+carrito = [] #Lista vacía
+cantidad=int(input(f"Ingrese la cantidad de productos que deseas agregar: "))
+print("")
+#Agregar artículos al carrito
+for i in range(cantidad):
+    nombre= input("Ingrese el nombre del producto: ")
+    cantidad = int(input(f"Ingrese el precio del producto: {nombre} "))
+    precio = int(input(f"Ingrese la cantidad del producto: {nombre} "))
+    carrito.append([nombre,cantidad,precio]) #Agregar contenido a la lista vacía
+    print("Producto agregado al carrito")
+    print("")
+
+#print(f"Productos agregados al carrito:\n {carrito}")
+contador = 0
+for i in carrito: #Imprime la lista de los productos desde la posición 0 hasta la 2
+    print("Nombre ", i[0])
+    print("Precio ", i[1])
+    print("Cantidad ", i[2])
+    print("")
+contador = contador +1
+
